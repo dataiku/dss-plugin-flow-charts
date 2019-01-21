b15264689e8f905a11e0c566669c70a827b87f62
admin <admin>
2019-01-21 03:59:38 -0500

    Edited file 'webapps/full-sankey-chart/app.js'

--- webapps/full-sankey-chart/app.js
+++ webapps/full-sankey-chart/app.js
@@ -46,11 +46,6 @@ function load() {
    
     let chart = new google.visualization.Sankey(document.getElementById('sankey'));
     chart.draw(data);
-//     google.visualization.events.addListener(chart, 'select', function() {
-//         if (chart.getSelection()[0].name) {
-//             loadState(chart.getSelection()[0].name.trim())
-//         }
-//     });
 }
     
 })()
